d354ef4d684550fcfdb3eb0807bedf07ea5e3132
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-09 20:45:37 +0900

    feat(ProductList): 물품의 리스트를 가지는 클래스를 생성한다
    물품을 나타내는 Product의 이름을 키로 가지고 Product를 value로 가지는
    LinkedHashMap을 관리하는 클래스를 생성한다. 데이터들을 ,로 구분하여
    Product객체로 생성한 후,  해시에  넣어주는 함수 initProducts를 생성한다.

---
+++ src/main/java/store/ProductList.java
@@ -0,0 +1,61 @@
+package store;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class ProductList {
+    LinkedHashMap<String, Product> products = new LinkedHashMap<>();
+
+    public ProductList(List<String> productList) {
+        initProducts(productList);
+    }
+
+    private void addProductToProducts(String[] productInfo) {
+        String name = productInfo[0];
+        int price = Integer.parseInt(productInfo[1]);
+        int quantity = Integer.parseInt(productInfo[2]);
+        String promotion = productInfo[3];
+        if (isExist(name)) {
+            modifyProduct(name, promotion, quantity, price);
+        }
+        if (!isExist(name)) {
+            addNewProduct(name, promotion, quantity, price);
+        }
+    }
+
+    private void initProducts(List<String> productList) {
+        for (String rawProduct : productList) {
+            String[] productInfo = rawProduct.split(",");
+            addProductToProducts(productInfo);
+        }
+    }
+
+    private boolean isExist(String name) {
+        if (products.containsKey(name)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void addNewProduct(String name, String promotion, int quantity, int price) {
+        Product product;
+        if (!Objects.equals(promotion, "null")) {
+            product = new Product(name, quantity, price, promotion);
+        } else {
+            product = new Product(name, quantity, price);
+        }
+        this.products.put(name, product);
+    }
+
+    private void modifyProduct(String name, String promotion, int quantity, int price) {
+        Product product = products.get(name);
+        if (Objects.equals(promotion, "null"))
+            product.addQuantity(quantity);
+        if (!Objects.equals(promotion, "null")) {
+            product.addEventQuantity(quantity);
+            this.products.put(name, product);
+        }
+    }
+}
