b19fd92af594e5c1820d2de2b8e9744c2ba2dae0
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-10 19:55:12 +0900

    feat(GetDataFromFile): promotions.md에 들어있는 값을 읽는 함수를 만든다
    md파일에 저장된 데이터들을 가져오는 함수를 추가한다.

--- src/main/java/store/GetDataFromFile.java
+++ src/main/java/store/GetDataFromFile.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class GetDataFromFile {
     String productFilePath = "src/main/resources/products.md";  // Path to the markdown file
+    String promotionFilePath = "src/main/resources/promotions.md";
 
     public GetDataFromFile() {
     }
@@ -15,6 +16,15 @@ public class GetDataFromFile {
         try {
             List<String> productData = Files.readAllLines(Paths.get(productFilePath));
             return productData;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("파일을 불러 오지 못했습니다");
+        }
+    }
+
+    public List<String> getPromotionInfo() {
+        try {
+            List<String> promotionData = Files.readAllLines(Paths.get(promotionFilePath));
+            return promotionData;
         } catch (IOException e) {
             throw new IllegalArgumentException("파일을 불러 오지 못했습니다");
         }
