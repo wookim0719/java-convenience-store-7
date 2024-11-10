16727b867ee3cf466446f9bfdac0d6fcf698e56a
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-10 19:59:14 +0900

    feat(Application): 클래스들의 객체들을 생성하여 연결한다
    물품의 재고를 관리하는 객체, 출력을 담당하는 객체를 생성하여 연결한다.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -3,5 +3,10 @@ package store;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GetDataFromFile data = new GetDataFromFile();
+        Promotion.setPromotionFeature(data.getPromotionInfo());
+        ProductList list = new ProductList(data.getProductsInfo());
+        OutputVIew view = new OutputVIew();
+        view.printProduct(list);
     }
 }
